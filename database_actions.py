9f2115865f7cd554a6f03eeda1f2c7b4522650f9
Jose <mezamenj@oregonstate.edu>
2024-07-21 01:51:26 -0700

    Moved login and create-quiz queries here

--- database_actions.py
+++ database_actions.py
@@ -1,3 +1,44 @@
-cmd = ("INSERT INTO employees "
-                "(first_name, last_name, hire_date, gender, birth_date) "
-                "VALUES (%s, %s, %s, %s, %s)")
+from database_connector import connect
+
+def login_query(email, password):
+  try:
+      conn = connect()
+      cursor = conn.cursor()
+
+      # Fetch the employer ID where email and password match
+      query = "SELECT ID FROM Employer WHERE Email = %s AND Password = %s"
+      cursor.execute(query, (email, password))
+
+      result = cursor.fetchone()  # This will be a tuple (ID,) or None if no match is found
+
+      return result
+
+  except mysql.connector.Error as err:
+      return {'error': str(err)}
+
+  finally:
+      cursor.close()
+      conn.close()
+
+def create_account_query(email, password):
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        
+        # Prepare and execute SQL query
+        query = "INSERT INTO employer (email, password) VALUES (%s, %s)"
+        cursor.execute(query, (email, password))
+        
+        # Commit the transaction
+        conn.commit()
+
+        # Get the last inserted ID
+        employer_id = int(cursor.lastrowid)
+        return {'message': 'success', 'employer_id': employer_id}
+
+    except mysql.connector.Error as err:
+        return {'error': str(err)}
+
+    finally:
+        cursor.close()
+        conn.close()
