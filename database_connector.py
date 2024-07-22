3bc6d5a58c61853d00f8f3e01abc3351925c890c
Jose <mezamenj@oregonstate.edu>
2024-07-21 23:34:34 -0700

    Implemented db class structure

--- database_connector.py
+++ database_connector.py
@@ -1,25 +1,34 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
 
-def connect(command, data):
+load_dotenv()
+
+class DatabaseConnection:
     """
     Initializes MySQL server connection, runs SQL command, and closes server connection.
     """
-    cnx = mysql.connector.connect(user="osuadmin",
+    def __init__(self):
+        self.connection = mysql.connector.connect(user="osuadmin",
                                 password=os.environ.get('password'),
                                 host="software-quiz.mysql.database.azure.com",
-                                port=3306, database="{your_database}",
-                                ssl_ca="{ca-cert filename}",
-                                ssl_disabled=False)
-    cursor = cnx.cursor()
+                                port=3306, database="quiz")
 
-    # Insert data with SQL command
-    cursor.execute(command, data)
+    def execute(self, command, data):
+        if self.connection is None:
+            print("Cannot execute query: Database connection was not initialized!")
+            return
+        if command is None or len(command.strip()) == 0:
+            print("Cannot execute query: Invalid query provided!")
+            return
+        cursor = self.connection.cursor()
 
-    # Make sure data is committed to the database
-    cnx.commit()
+        # Insert data with SQL command
+        cursor.execute(command, data)
 
-    cursor.close()
-    cnx.close()
+        # Make sure data is committed to the database
+        self.connection.commit()
+        return cursor
 
-connect()
+    def close(self):
+        self.connection.close()
