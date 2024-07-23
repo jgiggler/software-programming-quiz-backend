ba91c389ddbffa3653a8fd4b2d9c1797fa3e842f
Jose <mezamenj@oregonstate.edu>
2024-07-22 23:00:05 -0700

    Fixed errors with retrieving data, creating still works too

--- database_connector.py
+++ database_connector.py
@@ -5,9 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class DatabaseConnection:
-    """
-    Initializes MySQL server connection, runs SQL command, and closes server connection.
-    """
     def __init__(self):
         self.connection = mysql.connector.connect(user="osuadmin",
                                 password=os.environ.get('password'),
@@ -15,20 +12,14 @@ class DatabaseConnection:
                                 port=3306, database="quiz")
 
     def execute(self, command, data):
-        if self.connection is None:
-            print("Cannot execute query: Database connection was not initialized!")
-            return
-        if command is None or len(command.strip()) == 0:
-            print("Cannot execute query: Invalid query provided!")
-            return
         cursor = self.connection.cursor()
-
-        # Insert data with SQL command
-        cursor.execute(command, data)
-
-        # Make sure data is committed to the database
-        self.connection.commit()
-        return cursor
+        try:
+            cursor.execute(command, data)
+            return cursor
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
     def close(self):
-        self.connection.close()
+        if self.connection:
+            self.connection.close()
